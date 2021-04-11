6cbb472de8e02d6f5206cef4ffc2995d4922a075
Samantha Martorana <samanthamartorana7@gmail.com>
2021-04-11 11:28:00 -0500

    clock can tick

--- src/views/dashboard/elements/clock.tsx
+++ src/views/dashboard/elements/clock.tsx
@@ -8,6 +8,8 @@ export default function Clock(props: any) {
   const [deleted, setDeleted] = useState(false);
   console.log(props.slices);
 
+  let colors = ["grey", "grey", "grey", "grey", "grey", "grey"];
+
   let pieData = [];
   let sliceY = 100 / props.slices;
   for (let i = 0; i < props.slices; i++) {
@@ -31,10 +33,42 @@ export default function Clock(props: any) {
             />
           }
         >
-          <VictoryPie data={pieData} />
+          <VictoryPie
+            data={pieData}
+            style={{
+              data: {
+                fillOpacity: 0.9,
+                stroke: "white",
+                fill: "grey",
+                strokeWidth: 3,
+              },
+              labels: {
+                fill: "white",
+              },
+            }}
+            events={[
+              {
+                target: "data",
+                eventHandlers: {
+                  onClick: () => {
+                    return [
+                      {
+                        target: "data",
+                        mutation: ({ style }) => {
+                          return style.fill === "black"
+                            ? null
+                            : { style: { fill: "black" } };
+                        },
+                      },
+                    ];
+                  },
+                },
+              },
+            ]}
+          />
         </Card>
       ) : (
-        <div></div>
+        <span></span>
       )}
     </div>
   );

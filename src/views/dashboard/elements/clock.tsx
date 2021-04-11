e7d9044002c6e45a4467a12f15ded86e0c41788d
Samantha Martorana <samanthamartorana7@gmail.com>
2021-04-11 09:22:19 -0500

    init clock

---
+++ src/views/dashboard/elements/clock.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Pie, { ProvidedProps, PieArcDatum } from "@visx/shape/lib/shapes/Pie";
+
+export default function Clock() {
+  return <div className="App"></div>;
+}
